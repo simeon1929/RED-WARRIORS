37e6f620139a788346c7fec4a1d8f00467f23b76
HR <HR@Simeon-PC.batch.lan>
2025-10-26 10:05:09 +0800

    Updated files

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$(window).scroll(function() 
+        {
+        if ($(this).scrollTop() > 100)
+           $('header').addClass("fixedSticky");
+        else
+           $('header').removeClass("fixedSticky");
+});
+
+$(document).ready(function() {
+        $("nav a").on("click", function (event) {
+          event.preventDefault();
+          $("html,body").animate({scrollTop: $(this.hash).offset().top - 90}, 100);
+        });
+        
+});
+
+/*Menu Onclick*/
+let sideMenuToggle = $(".triger");
+let sideMenu = $("nav");
+if (sideMenuToggle.length) {
+    sideMenuToggle.on("click", function () {
+        $("body, header").addClass("activeMenu");
+        sideMenu.addClass("activeMenu");
+        $(function () {
+            setTimeout(function () {
+                $("#close_side_menu").fadeIn(300);
+            }, 300);
+        });
+    })
+}
+
+$(".close, nav a").on("click", function () {
+        $("body, header").removeClass("activeMenu");
+        sideMenu.removeClass("activeMenu");
+        
+        $(() => {
+            setTimeout(() => {
+                $("#close_side_menu").fadeOut();
+            }, 100);
+        });
+});
